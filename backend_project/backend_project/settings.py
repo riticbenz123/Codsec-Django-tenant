ffe548cbef39329a995eb5ba30b51320b43faff5
riticbenz123 <ritikbyanjankar@gmail.com>
2025-11-16 09:04:43 +0545

    added secret key

--- backend_project/backend_project/settings.py
+++ backend_project/backend_project/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -173,6 +174,10 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
+    #jwt secret key
+    'SIGNING_KEY': env('JWT_SECRET_KEY', default=None),
+    # secret algorithm
+    'ALGORITHM': 'HS256',
 }
 
 
